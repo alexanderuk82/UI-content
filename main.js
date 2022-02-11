26ac25af9bf35b28d64f8c1f71b18fb84bd3e9e4
Alexander Burgos <77622127+alexanderuk82@users.noreply.github.com>
2022-02-11 17:12:23 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-//!Search by categories
+/****===BTN OPTIONS FORM */
 
-// const options = {
-//     animationDuration: 0.5, // in seconds
-//     callbacks: {
-//         onFilteringStart: function () {},
-//         onFilteringEnd: function () {},
-//         onShufflingStart: function () {},
-//         onShufflingEnd: function () {},
-//         onSortingStart: function () {},
-//         onSortingEnd: function () {},
-//     },
-//     controlsSelector: '', // Selector for custom controls
-//     delay: 0, // Transition delay in ms
-//     delayMode: 'progressive', // 'progressive' or 'alternate'
-//     easing: 'ease-out',
-//     filter: 'all', // Initial filter
-//     filterOutCss: {
-//         // Filtering out animation
-//         opacity: 0,
-//         transform: 'scale(0.5)',
-//     },
-//     filterInCss: {
-//         // Filtering in animation
-//         opacity: 0,
-//         transform: 'scale(1)',
-//     },
-// }
-// const filterizr = new Filterizr('.checkbox', options)
+const btnOpt = document.querySelectorAll('.option-btn')
+
+btnOpt.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active')
+    })
+})
